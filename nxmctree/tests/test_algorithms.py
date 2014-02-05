f4d4996bd80bf4aad3918373c9f2d02cf7a0ab38
alex <argriffi@ncsu.edu>
2014-02-05 15:09:39 -0500

    add a test with full sparsity

--- nxmctree/tests/test_algorithms.py
+++ nxmctree/tests/test_algorithms.py
@@ -7,7 +7,8 @@ brute force implementations.
 """
 from __future__ import division, print_function, absolute_import
 
-from numpy.testing import run_module_suite, assert_equal, assert_allclose
+from numpy.testing import (run_module_suite, assert_,
+        assert_equal, assert_allclose)
 
 import nxmctree
 from nxmctree.nputil import (
@@ -20,60 +21,64 @@ from nxmctree import (
 from nxmctree.puzzles import gen_random_systems
 
 
-def test_without_sparsity():
-    # Test a special case of no data and no sparsity in the model.
+def test_full_sparsity():
+    # Test a special case of full sparsity in the system.
+    pzero = 1
+    for args in gen_random_systems(pzero):
+        T, e_to_P, r, r_prior, node_feas = args
+
+        for efficiency in 'dynamic', 'brute':
+            for variant in 'likelihood', 'feasibility':
+
+                # likelihood or feasibility
+                likelihood = get_likelihood(*args,
+                        efficiency=efficiency, variant=variant)
+                assert_(not likelihood)
+
+                # state distributions or feasible sets at nodes
+                d = get_node_to_posterior_distn(*args,
+                        efficiency=efficiency, variant=variant)
+                assert_(not any(d.values()))
+
+                # joint state distributions at edge endpoints
+                d = get_edge_to_joint_posterior_distn(*args,
+                        efficiency=efficiency, variant=variant)
+                for edge in T.edges():
+                    assert_(not d[edge].edges())
+
+
+def test_zero_sparsity():
+    # Test a special case of no sparsity in the system.
     pzero = 0
     for args in gen_random_systems(pzero):
         T, e_to_P, r, r_prior, node_feas = args
 
-        # likelihood
-        dynamic = get_likelihood(*args)
-        brute = get_likelihood(*args, efficiency='brute')
-        assert_allclose(dynamic, 1)
-        assert_allclose(brute, 1)
+        for efficiency in 'dynamic', 'brute':
 
-        # feasibility
-        dynamic = get_likelihood(*args, variant='feasibility')
-        brute = get_likelihood(*args, efficiency='brute', variant='feasibility')
-        assert_equal(dynamic, True)
-        assert_equal(brute, True)
+            # likelihood
+            likelihood = get_likelihood(*args, efficiency=efficiency)
+            assert_allclose(likelihood, 1)
 
-        # state distributions at nodes
-        dynamic = get_node_to_posterior_distn(*args)
-        brute = get_node_to_posterior_distn(*args, efficiency='brute')
-        for v in set(node_feas):
-            assert_equal(set(dynamic[v]), node_feas[v])
-            assert_equal(set(brute[v]), node_feas[v])
+            # feasibility
+            feasibility = get_likelihood(*args,
+                efficiency=efficiency, variant='feasibility')
+            assert_equal(feasibility, True)
 
-        # state feasibility at nodes
-        dynamic = get_node_to_posterior_distn(*args, variant='feasibility')
-        brute = get_node_to_posterior_distn(*args, efficiency='brute',
-                variant='feasibility')
-        for v in set(node_feas):
-            assert_equal(dynamic[v], node_feas[v])
-            assert_equal(brute[v], node_feas[v])
+            for variant in 'likelihood', 'feasibility':
 
-        # joint state distributions at edge endpoints
-        dynamic = get_edge_to_joint_posterior_distn(*args)
-        brute = get_edge_to_joint_posterior_distn(*args, efficiency='brute')
-        for edge in T.edges():
-            dynamic_edges = set(dynamic[edge].edges())
-            brute_edges = set(dynamic[edge].edges())
-            desired_edges = set(e_to_P[edge].edges())
-            assert_equal(dynamic_edges, desired_edges)
-            assert_equal(brute_edges, desired_edges)
+                # state distributions at nodes
+                d = get_node_to_posterior_distn(*args,
+                        efficiency=efficiency, variant=variant)
+                for v in set(node_feas):
+                    assert_equal(set(d[v]), node_feas[v])
 
-        # joint state feasibility at edge endpoints
-        dynamic = get_edge_to_joint_posterior_distn(*args,
-                variant='feasibility')
-        brute = get_edge_to_joint_posterior_distn(*args, efficiency='brute',
-                variant='feasibility')
-        for edge in T.edges():
-            dynamic_edges = set(dynamic[edge].edges())
-            brute_edges = set(dynamic[edge].edges())
-            desired_edges = set(e_to_P[edge].edges())
-            assert_equal(dynamic_edges, desired_edges)
-            assert_equal(brute_edges, desired_edges)
+                # joint state distributions at edge endpoints
+                d = get_edge_to_joint_posterior_distn(*args,
+                        efficiency=efficiency, variant=variant)
+                for edge in T.edges():
+                    observed_edges = set(d[edge].edges())
+                    desired_edges = set(e_to_P[edge].edges())
+                    assert_equal(observed_edges, desired_edges)
 
 
 def test_dynamic_vs_brute():
