4f4d35466fdcf85da6f63d236baa0834102e9a1a
alex <argriffi@ncsu.edu>
2014-02-17 12:57:38 -0500

    docstring

--- nxmctree/wrappers.py
+++ nxmctree/wrappers.py
@@ -35,11 +35,57 @@ def get_likelihood(T, edge_to_P, root,
     return f(T, edge_to_P, root, root_prior_distn, node_to_data_feasible_set)
 
 
-#TODO add an informative docstring
 def get_node_to_posterior_distn(T, edge_to_P, root,
         root_prior_distn, node_to_data_feasible_set,
         efficiency=DYNAMIC, variant=LIKELIHOOD):
     """
+    For each node get a posterior distribution or posterior feasible set.
+
+    The posterior feasible state set for each node is determined through
+    several data sources: transition matrix sparsity,
+    state restrictions due to observed data at nodes,
+    and state restrictions at the root due to the support of its prior
+    distribution.
+
+    Parameters
+    ----------
+    T : directed networkx tree graph
+        Edge and node annotations are ignored.
+    edge_to_adjacency : dict
+        A map from directed edges of the tree graph
+        to networkx graphs representing state transition.
+        In likelihood mode the edge weights represent probabilities.
+        In feasibility mode the edges are interpreted as unweighted.
+        In both cases, missing edges are interpreted as transitions
+        that are infeasible due to properties of the process or model.
+    root : hashable
+        This is the root node.
+        Following networkx convention, this may be anything hashable.
+    root_prior_distn : dict or set
+        In likelihood mode this is a prior distribution of states at the root.
+        In feasibility mode this is a set of feasible prior root states.
+    node_to_data_feasible_set : dict
+        Map from node to set of feasible states.
+        The feasibility could be interpreted as due to restrictions
+        caused by observed data.
+    efficiency : str in {'dynamic', 'brute'}, optional
+        The dynamic mode uses dynamic programming, while the brute force mode
+        uses enumeration.  The only reason to use the brute force mode
+        is for testing. (default: 'dynamic')
+    variant : str in {'likelihood', 'feasibility'}, optional
+        The likelihood mode uses probabilities and distributions,
+        whereas the feasibility mode is concerned only with
+        possibility vs. impossibility. (default: 'likelihood')
+
+    Returns
+    -------
+    node_to_posterior_distn : dict
+        The returned dict gives posterior information about each node.
+        In likelihood mode the per-node information consists of a posterior
+        distribution over states.
+        In feasibility mode the per-node information consists of the
+        posterior set of feasible states.
+
     """
     f = {
             (DYNAMIC, LIKELIHOOD) : (
