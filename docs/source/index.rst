cc4037a4af8eb922c8b10420e22234a6896673cc
alex <argriffi@ncsu.edu>
2014-02-28 13:28:36 -0500

    even more docs about making docs

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -12,7 +12,7 @@ Contents:
 .. toctree::
    :maxdepth: 4
 
-   nxmctree
+   apidocs/nxmctree
    glossary
 
 
--- make-docs.sh
+++ make-docs.sh
@@ -1,13 +1,13 @@
 
 # delete parts of the old build if present
 rm -rf /tmp/nxdocs
-rm -f docs/source/*.rst
+rm -rf docs/source/apidocs
 
 # copy the manually edited .rst files into the source directory
 cp docs/manually-edited/*.rst docs/source/
 
 # use sphinx to build the rest of the .rst api docs into the source directory
-sphinx-apidoc --separate -o docs/source nxmctree
+sphinx-apidoc --separate -o docs/source/apidocs nxmctree
 
 # use sphinx to build a static web page from the .rst files
 sphinx-build -b html docs/source /tmp/nxdocs
