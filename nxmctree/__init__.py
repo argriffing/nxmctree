1582741e08674a27917cd286121fdcfe21b3230d
alex <argriffi@ncsu.edu>
2014-03-11 23:54:58 -0400

    expose the lmap rather than the fset interface

--- nxmctree/__init__.py
+++ nxmctree/__init__.py
@@ -8,8 +8,8 @@ from numpy.testing import Tester
 test = Tester().test
 bench = Tester().bench
 
-from . import dynamic_fset_lhood
-from .dynamic_fset_lhood import *
+from . import dynamic_lmap_lhood
+from .dynamic_lmap_lhood import *
 
 from . import sampling
 from .sampling import *
@@ -18,7 +18,7 @@ from . import history
 from .history import *
 
 __all__ = ['test', 'bench']
-__all__ += dynamic_fset_lhood.__all__
+__all__ += dynamic_lmap_lhood.__all__
 __all__ += sampling.__all__
 __all__ += history.__all__
 
